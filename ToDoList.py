231bdf4301c6d56850acb868446629e3a8812692
Stephen Sohlin <ssohlin13@gmail.com>
2021-09-12 12:12:15 -0400

    widget layout fixed; next: add datetime picker

--- ToDoList.py
+++ ToDoList.py
@@ -44,7 +44,7 @@ def updateFile():
     my_file.close()
 
     #not needed 
-    confirm_label = Label(mainframe, text="Saved!", fg="#07e320", bg="#828282").grid(row=1, column=0, columnspan=3, pady=(10,10))
+    confirm_label = Label(mainframe, text="Saved!", fg="#07e320", bg="#828282").grid(row=0, column=0, columnspan=5, pady=(10,10))
 
 global task_list    
 global task_list_box
@@ -67,30 +67,30 @@ root.columnconfigure(2, weight=1)
 root.rowconfigure(2, weight=1)
 
 save_icon = PhotoImage(file="images/save-icon.png")
-add_icon = PhotoImage(file="images/add-button.png")
+add_icon = PhotoImage(file="images/add-icon.png")
 delete_icon = PhotoImage(file="images/trash-bin.png")
 #button that will save elements in listbox to a txt file
 save_button = tk.Button(mainframe, text="Save", image=save_icon, highlightbackground="#6e6e6e", borderwidth=0, command=updateFile)
 save_button.grid(row=0, column=1, sticky="N", padx=(50,10), pady=(15,15))
 
 #button to remove tasks when they select it in the listbox and select the delete button
-remove_tasks = tk.Button(mainframe, text="Delete", image=delete_icon, highlightbackground="#e01212", borderwidth=0, command=deleteTasks)
-remove_tasks.grid(row=0, column=2, sticky="N", padx=(200, 10), pady=(15,15))
+remove_tasks = tk.Button(mainframe, text="Delete", image=delete_icon, highlightbackground="#e01212", highlightthickness=0, bd=0, borderwidth=0, command=deleteTasks)
+remove_tasks.grid(row=0, column=3, sticky="N", padx=(200, 10), pady=(15,15))
 
 #button to add tasks
 add_tasks = tk.Button(mainframe, text="+", image=add_icon, highlightbackground="#0013b5", borderwidth=0, command=addTasks)
-add_tasks.grid(row=0, column=3, sticky="NE", pady=(15,15))
+add_tasks.grid(row=0, column=4, sticky="NE", pady=(15,15))
 
 #variable to capture the text in the entry box
 new_task = StringVar()
 
 #entry widget where user will type in tasks
 task_entry = Entry(mainframe, textvariable=new_task)
-task_entry.grid(row=2, column=0, columnspan=3)
+task_entry.grid(row=2, column=0, columnspan=5)
 
 #widget to hold tasks
 task_list_box = Listbox(mainframe, background="#18261c", fg="#ebedeb")
-task_list_box.grid(row=3, column=0, columnspan=3)
+task_list_box.grid(row=3, column=0, columnspan=5)
 
 #if file exists, read text and store into a list, otherwise create file and keep going
 if path.isfile(task_list_file):
